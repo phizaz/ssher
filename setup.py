831e11b422f3e62704d8bca7fc93d568b407ae35
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-06-11 17:33:04 +0700

    update the package name to 'sssh' to avoid collision

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-    name = 'ssher',
-    packages = ['ssher'],
-    version = '0.1',
+    name = 'sssh',
+    packages = ['sssh'],
+    version = '0.2',
     description = 'Stop remembering all the hosts when using SSH',
     author = 'Konpat Preechakul',
     author_email = 'the.akita.ta@gmail.com',
     url = 'https://github.com/phizaz/ssher', # use the URL to the github repo
-    download_url = 'https://github.com/phizaz/ssher/tarball/0.1', # I'll explain this in a second
+    download_url = 'https://github.com/phizaz/ssher/tarball/0.2', # I'll explain this in a second
     keywords = ['ssh'], # arbitrary keywords
     classifiers = [],
 )
