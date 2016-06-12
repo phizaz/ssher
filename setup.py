97d20a46719997c8aac419dd7f05c0a24390dd8e
Konpat Ta Preechakul <the.akita.ta@gmail.com>
2016-06-12 11:45:30 +0700

    version 0.4

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     entry_points={
         "console_scripts": ['sssh = sssh.ssher:main']
     },
-    version = '0.3',
+    version = '0.4',
     description = 'Stop remembering all the hosts when using SSH',
     author = 'Konpat Preechakul',
     author_email = 'the.akita.ta@gmail.com',
     url = 'https://github.com/phizaz/ssher', # use the URL to the github repo
-    download_url = 'https://github.com/phizaz/ssher/tarball/0.3', # I'll explain this in a second
+    download_url = 'https://github.com/phizaz/ssher/tarball/0.4', # I'll explain this in a second
     keywords = ['ssh'], # arbitrary keywords
     classifiers = [],
 )
